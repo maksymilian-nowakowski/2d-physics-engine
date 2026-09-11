21584541df3eb70d47d523208d42a1fd7ab42ca1
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-12 00:12:49 +0100

    Add test for Body force accumulation

--- tests/test_body.py
+++ tests/test_body.py
@@ -49,3 +49,23 @@ def test_invalid_restitution():
     for valid_restitution in (0, 1):
         body = Body(Vector2(0, 0), mass=1, radius=1, restitution=valid_restitution)
         assert body.restitution == valid_restitution
+
+def test_apply_force():
+    body = Body(Vector2(0, 0), mass=1, radius=1)
+    body.apply_force(Vector2(10, 5))
+
+    assert body.force == Vector2(10, 5)
+
+def test_multiple_forces_accumulate():
+    body = Body(Vector2(0, 0), mass=1, radius=1)
+    body.apply_force(Vector2(10, 5))
+    body.apply_force(Vector2(-3, 2))
+
+    assert body.force == Vector2(7, 7)
+
+def test_clear_forces():
+    body = Body(Vector2(0, 0), mass=1, radius=1)
+    body.apply_force(Vector2(10, 5))
+    body.clear_forces()
+
+    assert body.force == Vector2(0, 0)
