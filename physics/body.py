5320b3164bcfb55b61788518717e76b779819072
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-11 21:33:02 +0100

    Update Body constructuor

--- physics/body.py
+++ physics/body.py
@@ -1,8 +1,13 @@
+from physics.vector import Vector2
+
+
 class Body:
-    def __init__(self, position, mass, radius, restitution=0.5):
+    def __init__(self, position: Vector2, mass: float, radius: float, restitution: float=0.5):
         self.position = position
+        self.previous_position = position.copy()
         self.mass = mass
         self.radius = radius
         self.restitution = restitution
+        self.acceleration = Vector2(0, 0)
+        self.force = Vector2(0, 0)
 
-    
