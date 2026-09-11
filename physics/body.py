dff506743dc57aa072969104fec7d3b673523d6d
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-11 21:43:14 +0100

    Add Body clear forces

--- physics/body.py
+++ physics/body.py
@@ -14,10 +14,16 @@ class Body:
     def apply_force(self, force):
         self.force += force
 
+    def clear_forces(self):
+        self.force = Vector2(0, 0)
+
+
 
 if __name__ == "__main__":
     ball = Body(Vector2(0, 0), 1.0, 1.0)
     print(ball.force)
     ball.apply_force(Vector2(10, 0))
     ball.apply_force(Vector2(5, 0))
+    print(ball.force)
+    ball.clear_forces()
     print(ball.force)
