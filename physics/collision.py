eca58cf50baa701c48f3debe1d181a3a2ce48d0b
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-12 22:35:40 +0100

    Add input validation for check circle collision

--- physics/collision.py
+++ physics/collision.py
@@ -3,4 +3,8 @@ from physics.vector import Vector2
 
 
 def check_circle_collision(body_a, body_b):
+    if isinstance(body_a, Body) == False or isinstance(body_b, Body) == False:
+        raise ValueError("either arguement isn't a body")
+    if body_a == body_b:
+        raise ValueError("body cannot collide with itself")
     return body_a.position.distance_to(body_b.position) <= body_a.radius + body_b.radius
