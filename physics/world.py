eb985edce5de48c0a4d5dcac7a5a620427eb6e53
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-12 01:06:57 +0100

    Add World add body

--- physics/world.py
+++ physics/world.py
@@ -1,7 +1,14 @@
 from physics.vector import Vector2
-
+from physics.body import Body
 
 class World:
     def __init__(self, gravity=Vector2(0, 9.81)):
         self.bodies = []
         self.gravity = gravity
+
+    def add_body(self, body):
+        if isinstance(body, Body) == False:
+            raise ValueError("supplied object must be a Body")
+        if body in self.bodies:
+            raise ValueError("same body shouldn't be added twice")
+        self.bodies.append(body)
