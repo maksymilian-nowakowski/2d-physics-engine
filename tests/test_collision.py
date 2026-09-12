777db9d56c62f9384cb78d6b6f895a06d6c0c1c8
maksymilian-nowakowski <maks.nowakowski.pro@gmail.com>
2026-09-12 22:16:54 +0100

    Add test circles are colliding when touching

--- tests/test_collision.py
+++ tests/test_collision.py
@@ -10,3 +10,10 @@ def test_circles_are_not_colliding():
     body_b = Body(Vector2(11, 0), mass=1, radius=5)
 
     assert check_circle_collision(body_a, body_b) == False
+
+
+def test_circles_are_colliding_when_touching():
+    body_a = Body(Vector2(0, 0), mass=1, radius=5)
+    body_b = Body(Vector2(10, 0), mass=1, radius=5)
+
+    assert check_circle_collision(body_a, body_b) == True
